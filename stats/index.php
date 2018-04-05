a84d0175e6d69d67e6e4f00c814cdb0c70a5df6e
Ben Harris <ben@tilde.team>
2018-04-05 14:48:18 -0400

    some things

--- stats/index.php
+++ stats/index.php
@@ -1,6 +1,11 @@
 <?php
-if (time() - filemtime("stats.html") > 1 * 3600)
-    exec("./analyze_hdb_logs");
 
-echo file_get_contents("stats.html");
+if (!file_exists("stats.html") || time() - filemtime("stats.html") > 3600) {
+    echo shell_exec("./analyze_hdb_logs 2>&1");
+}
+
+if (file_exists("stats.html"))
+    echo file_get_contents("stats.html");
+else
+    echo "script didn't run. fix it.";
 
